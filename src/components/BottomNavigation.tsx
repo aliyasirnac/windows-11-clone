c9dfeab774bc430b9922d301ef82716295749d03
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2023-08-22 19:28:24 +0300

    folder tasarımı ve işlevselliği yapıldı, ileride daha iyi bi hale getireceğim

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function BottomNavigation() {
   return (
-    <div className="flex w-full space-x-2  bottom-0 h-10 justify-center absolute items-center bg-gradient-to-r from-gray-100 bg-opacity-50 to-gray-300">
-      <div className="w-8 h-8 hover:bg-white">
+    <div className="flex w-full space-x-2  bottom-0 h-10 justify-center absolute items-center bg-blue-400 bg-opacity-50">
+      <div className="w-8 h-8 hover:scale-105">
         <Image
           src={"/icons/icons8-windows-10.svg"}
           alt=""
@@ -12,7 +12,7 @@ export default function BottomNavigation() {
           height={32}
         />
       </div>
-      <div className="w-8 h-8 hover:bg-white">
+      <div className="w-8 h-8 hover:scale-105">
         <Image
           src={"/icons/icons8-microsoft-edge.svg"}
           alt=""
